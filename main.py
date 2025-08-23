481dcbbcbc89aef4d531911c1bbf07cc15082dd3
anuraag <anuraag.warudkar@gmail.com>
2025-08-23 07:08:14 +0000

    simplify model

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from tinygrad import Tensor, TinyJit, nn, GlobalCounters
+from tinygrad import Tensor, TinyJit, nn, GlobalCounters, dtypes
 import sys
-from tinygrad.helpers import trange
+from tinygrad.helpers import trange, getenv
 import youtokentome as yttm
 from pathlib import Path
 import math
 import random
-from typing import Tuple
+from examples.mlperf.lr_schedulers import CosineAnnealingLRWithWarmup 
+from extra.lr_scheduler import LRSchedulerGroup
 
 N_CTX = 256 
-D_emb = 128 
-D_head = 128 
+D_emb = 192 
+N_HEADS = 6 
+D_head =  D_emb // N_HEADS
 d_ff = 4 * D_emb
-n_layers = 4
+n_layers = 6 
 
 NL = "<|nl|>"
 
@@ -25,28 +27,38 @@ if not model_path.exists():
 bpe = yttm.BPE(model=str(model_path))
 
 class MLP:
-  def __init__(self):
-    self.w1 = nn.Linear(D_emb, d_ff, bias=False)
-    self.w2 = nn.Linear(D_emb, d_ff, bias=False)
-    self.w3 = nn.Linear(d_ff, D_emb, bias=False)
+  def __init__(self, d_ff=d_ff, dropout_p=0.0):
+    self.fc1 = nn.Linear(D_emb, d_ff, bias=True)
+    self.fc2 = nn.Linear(d_ff, D_emb, bias=True)
+    self.dropout_p = dropout_p
+
   def __call__(self, x: Tensor) -> Tensor:
-    return self.w3(self.w1(x) * self.w2(x).sigmoid()).dropout(p=0.1)
+    x = self.fc1(x)     # (B, T, d_ff)
+    x = x.gelu()        # nonlinearity
+    x = self.fc2(x)     # (B, T, D_emb)
+    if self.dropout_p and self.dropout_p > 0.0:
+      x = x.dropout(p=self.dropout_p)
+    return x
 
 class SelfAttention:
   def __init__(self):
-    self.q = nn.Linear(D_emb, D_head, bias=False)
-    self.k = nn.Linear(D_emb, D_head, bias=False)
-    self.v = nn.Linear(D_emb, D_head, bias=False)
-    self.o = nn.Linear(D_head, D_emb, bias=False)
-  def __call__(self, x: Tensor, mask: Tensor) -> Tensor:
-    q = self.q(x)
-    k = self.k(x)
-    v = self.v(x) # all of shape (B, T, D_head)
+    self.qkv = nn.Linear(D_emb, 3*D_emb, bias=True)
+    self.proj_out = nn.Linear(D_emb, D_emb, bias=True)
+  def __call__(self, x: Tensor, mask: Tensor) -> Tensor: 
+    B, T, _ = x.shape
+    # (B, T, 3*D) → (B, T, 3, H, D_HEAD) → (3, B, H, T, D_HEAD)
+    qkv = self.qkv(x).reshape(B, T, 3, N_HEADS, D_head).permute(2,0,3,1,4)
+    q, k, v = qkv[0], qkv[1], qkv[2]                       # each (B, H, T, D_HEAD)
 
-    scores: Tensor = (q @ k.transpose(1,2)) * (1.0 / math.sqrt(D_head)) #  (B, T, T)
-    weights = scores.masked_fill(mask == 0, -1e9).softmax().dropout(p=0.1) # (B, T, T)
+    # scaled dot-product
+    att = (q @ k.transpose(2,3)) * (1.0 / math.sqrt(D_head))   # (B, H, T, T)
+    att = att.masked_fill(mask == 0, -1e9).softmax()
 
-    return self.o(weights @ v) # (B, T, D_emb) same as input, so we can chain to next module
+    # (B, H, T, T) @ (B, H, T, D_HEAD) → (B, H, T, D_HEAD)
+    out = att @ v
+    out = out.transpose(1,2).reshape(B, T, D_emb)              # concat heads
+
+    return self.proj_out(out)          # (B, T, D_emb)
 
 class Block:
   def __init__(self):
@@ -55,8 +67,8 @@ class Block:
     self.ln2 = nn.RMSNorm(D_emb)
     self.mlp = MLP()
   def __call__(self, x: Tensor, mask: Tensor) -> Tensor:
-    x += self.attn(self.ln1(x), mask).dropout(p=0.1)
-    x += self.mlp(self.ln2(x)).dropout(p=0.1)
+    x += self.attn(self.ln1(x), mask)
+    x += self.mlp(self.ln2(x))
     return x
 
 class Transformer: 
@@ -65,19 +77,18 @@ class Transformer:
     self.wpe = nn.Embedding(N_CTX, D_emb) # positional embeddings (learned, added to wte)
     self.blocks = [Block() for _ in range(n_layers)]
     self.ln_f = nn.RMSNorm(D_emb)
-    self.head_bias = Tensor.zeros((bpe.vocab_size(),), requires_grad=True)
   def __call__(self, token_ids: Tensor) -> Tensor:
     _, T = token_ids.shape
     pos = Tensor.arange(T).unsqueeze(0)
-    x = (self.wte(token_ids) + self.wpe(pos)).dropout(p=0.1)
-    mask = Tensor.tril(Tensor.ones((T,T))).unsqueeze(0) # (1, T, T)
+    x = (self.wte(token_ids) + self.wpe(pos))
+    mask = Tensor.tril(Tensor.ones((T,T))).unsqueeze(0).unsqueeze(1) # (1, T, T)
     for blk in self.blocks: 
       x = blk(x, mask)
     x = self.ln_f(x) 
-    logits = (x @ self.wte.weight.transpose(1,0)) + self.head_bias
+    logits = (x @ self.wte.weight.transpose(1,0)) 
     return logits
 
-def make_blocks(token_ids, block_size=256, test_frac=0.1, stride=64):
+def make_blocks(token_ids, block_size=256, test_frac=0.1, stride=192):
   L = len(token_ids)
   starts = list(range(0, L - block_size - 1, stride))
   X = [token_ids[i:i+block_size] for i in starts]
@@ -86,7 +97,7 @@ def make_blocks(token_ids, block_size=256, test_frac=0.1, stride=64):
   random.shuffle(pairs)
   X, y = zip(*pairs)
   split = int(len(X) * (1.0 - test_frac))
-  return Tensor(list(X[:split])), Tensor(list(y[:split])), Tensor(list(X[split:])), Tensor(list(y[split:]))
+  return Tensor(list(X[:split]), dtype=dtypes.int32), Tensor(list(y[:split]), dtype=dtypes.int32), Tensor(list(X[split:]), dtype=dtypes.int32), Tensor(list(y[split:]), dtype=dtypes.int32)
 
 
 def generate(epoch: int, prompt: str = "", max_tokens:int = 256, temperature: float = 1.0, top_k: int = 50):
@@ -128,6 +139,7 @@ def generate(epoch: int, prompt: str = "", max_tokens:int = 256, temperature: fl
     sys.stdout.write(prompt)
     sys.stdout.flush()
 
+  @Tensor.train(False)
   def _infer(input_ids: Tensor):
     logits = model(input_ids)            
     next_token_logits = logits[0, -1]   
@@ -157,23 +169,14 @@ def generate(epoch: int, prompt: str = "", max_tokens:int = 256, temperature: fl
     
 if __name__ == "__main__":
   if len(sys.argv) > 1 and sys.argv[1] == "generate":
-    generate(epoch=999, prompt="to be or not to be ")
+    generate(epoch=4999, prompt="to be or not to be ")
     exit()
+  
+  Path("checkpoints").mkdir(exist_ok=True)
 
   model = Transformer()
   
-  # print param count for fun
-  print("parameter count:", sum([p.numel() for p in nn.state.get_parameters(model)]))
-  
-  # need to skip weight decay on some parameters
-  all_params = nn.state.get_parameters(model)
-  no_wd = [v for k,v in nn.state.get_state_dict(model).items() if ("bias" in k) or ("ln" in k) or ("wte" in k) or ("wpe" in k)]
-  wd_params = [p for p in all_params if p not in set(no_wd)]
-  print(len(no_wd), len(wd_params))
-
-  opt_wd = nn.optim.AdamW(wd_params, lr=1e-4, b1=0.9, b2=0.95, weight_decay=0.01)
-  opt_no_wd = nn.optim.Adam(no_wd, lr=1e-4, b1=0.9, b2=0.95)
-  optimizer = nn.optim.OptimizerGroup(opt_wd, opt_no_wd)
+  opt = nn.optim.Adam(nn.state.get_parameters(model), lr=3e-4)
 
   token_ids = bpe.encode([tiny_shakes_text], output_type=yttm.OutputType.ID)[0]
   X_train, Y_train, X_test, Y_test = make_blocks(token_ids)
@@ -182,27 +185,28 @@ if __name__ == "__main__":
   @TinyJit
   @Tensor.train()
   def train_step() -> Tensor:
-    optimizer.zero_grad()
-    samples = Tensor.randint(128, high=X_train.shape[0])
-    loss = model(X_train[samples]).sparse_categorical_crossentropy(Y_train[samples]).mean().backward()
-    return loss.realize(*optimizer.schedule_step())
+    opt.zero_grad()
+    samples = Tensor.randint(getenv("BS", 128), high=int(X_train.shape[0]))
+    loss = model(X_train[samples]).sparse_categorical_crossentropy(Y_train[samples]).backward()
+    return loss.realize(*opt.schedule_step())
 
   @TinyJit
+  @Tensor.train(False)
   def get_test_acc() -> Tensor:
-    val_loss = model(X_test).sparse_categorical_crossentropy(Y_test).mean()
+    val_loss = model(X_test).sparse_categorical_crossentropy(Y_test)
     val_ppl = val_loss.exp()
     return val_ppl
 
   test_acc = float('nan')
-  for i in (t:=trange(1000)):
+  for i in (t:=trange(5000)):
     GlobalCounters.reset()
     loss = train_step() 
     losses.append(loss.item())
     if i%10 == 9:
       test_acc = get_test_acc().item()
-    if i%100 == 99:
+    if i%500 == 499:
       state_dict = nn.state.get_state_dict(model)
       nn.state.safe_save(state_dict, f"checkpoints/model{i}.safetensors")
-    t.set_description(f"loss: {loss.item():6.2f} test_accuracy(ppl): {test_acc:.2f}")
+    t.set_description(f"loss: {loss.item():6.2f} val_ppl: {test_acc:.2f}")
 
   open("losses.txt", "w").write(','.join([str(x) for x in losses]))
